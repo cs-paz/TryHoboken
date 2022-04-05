6aed2f0d35117521ea47a1463c2f47714dcff7d5
Christian Szablewski-Paz <32683410+cs-paz@users.noreply.github.com>
2022-04-04 21:44:48 -0400

    added bars list page

--- frontend/src/pages/bars/Bars.js
+++ frontend/src/pages/bars/Bars.js
@@ -1,9 +1,42 @@
-const Bars = () => {
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const BarsPage = () => {
+  const [bars, setBars] = useState([]);
+
+  useEffect(async () => {
+    const { data } = await axios.get("http://localhost:3001/bars");
+    setBars(data);
+  }, []);
+
+  return (
+    <>
+      {bars.map((bar) => {
+        return (
+          <div key={bar._id}>
+            <Bar bar={bar} />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+const Bar = (bar) => {
+  const link = "/bars/" + bar.bar._id;
   return (
     <div>
-      <h1>Bars</h1>
+      <h1>
+        <Link to={link}>{bar.bar.name}</Link>
+      </h1>
+      <p>{bar.bar.description}</p>
+      <p>{bar.bar.type}</p>
+      <p>{bar.bar.address}</p>
+      <p>{bar.bar.description}</p>
+      <a href={bar.bar.url}>{bar.bar.url}</a>
     </div>
   );
 };
 
-export default Bars;
+export default BarsPage;
